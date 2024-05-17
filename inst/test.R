963d88b1a875e161110fa4a68efa17b6dc5a1020
dwjak123lkdmaKOP <Maximilian.Pichler@biologie.uni-regensburg.de>
2024-05-17 14:26:45 +0200

    update set_weights

--- inst/test.R
+++ inst/test.R
@@ -73,18 +73,42 @@ response = "dbh"
 
 mortFP(dbh, Species, nTree+0.00001, self$parMort, pred_morth[,i,], AL) #.unsqueeze(3)
 
-initCohort = CohortMat$new(dims = c(100, 30, 2),
-                           dbh = array(100, dim = c(100, 30, 2)),
-                           trees = array(50, dim = c(100, 30, 2)),
-                           sp = 15)
+initCohort = CohortMat$new(dims = c(100, 1, 10),
+                           dbh = array(1, dim = c(100, 1, 10)),
+                           trees = array(1, dim = c(100, 1, 10)),
+                           sp = 1)
 
-finn = FINN$new(sp = 15L, env = 2L, device = "cpu", which = "all")
+finn = FINN$new(sp = 1L, env = 2L, device = "cpu", which = "all" ,parGrowth = matrix(555, 1, 2), parGrowthEnv = list(matrix(5.0, 1, 2)), parMortEnv = list(matrix(0, 1, 2)), parRegEnv = list(matrix(10, 1, 2)))
+pred =
+  finn$predict(initCohort$dbh, initCohort$trees, initCohort$species,
+               env = torch::torch_randn(size = c(100, 200, 2)),patches = 1)
+plot(torch::as_array(pred[[1]]$data())[1,,1])
+
+
+
+
+
+
+pred =
+finn$predict(initCohort$dbh, initCohort$trees, initCohort$species,
+             env = torch::torch_randn(size = c(100, 31, 2)),patches = 30)
+
+
 finn$fit(initCohort = initCohort,epochs = 20,
          X =  torch::torch_randn(size = c(100, 31, 2)),
          Y = torch::torch_ones(size = c(100, 31, 15, 2)),batch_size = 100L,
          patches = 30L, learning_rate = 0.1)
 
-
+initCohort = CohortMat$new(dims = c(100, 30, 2),
+                           dbh = array(100, dim = c(100, 30, 2)),
+                           trees = array(50, dim = c(100, 30, 2)),
+                           sp = 3)
+finn = FINN$new(sp = 3L, env = 2L, device = "cpu", which = "all", parMort = matrix(0, 3, 3), parGrowth = matrix(0.1, 3, 2))
+finn$parMort
+pred =
+  finn$predict(initCohort$dbh, initCohort$trees, initCohort$species,
+               env = torch::torch_randn(size = c(100, 31, 2)),patches = 30)
+plot(torch::as_array(pred[[1]]$data())[1,,1])
 
 
 
