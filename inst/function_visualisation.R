e6948bd6f0de329a9f2898966c1f1c18ff53c3e8
ykaeber <y.kaeber@posteo.de>
2024-05-22 17:07:56 +0200

    small changes

--- inst/function_visualisation.R
+++ inst/function_visualisation.R
@@ -2,15 +2,17 @@
 library(ggplot2)
 library(data.table)
 library(torch)
-################################################################################
-## create pdf file
-################################################################################
-# Open the existing PDF file in append mode
-pdf("function_vis.pdf")
+library(FINN)
 
-################################################################################
-## sensitivity of height_P function
-################################################################################
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+## create pdf ####
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+# Open the existing PDF file in append mode
+# pdf("function_vis.pdf")
+
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+## height ####
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
 test_dt =
   data.table(
     expand.grid(
@@ -33,10 +35,9 @@ p_height <-
     geom_label(aes(label = parHeight, color = parHeight), test_dt[,.(height = max(height),dbh = max(dbh)),by=parHeight])
 print(p_height)
 
-################################################################################
-## sensitivity of BA_stem function
-################################################################################
-
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+## BA_stem ####
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
 # Generate test data
 test_dt <- data.table(
   dbh = seq(0, 500, 1)
@@ -53,16 +54,13 @@ p_BA_stem <- ggplot(test_dt, aes(x = dbh, y = basal_area)) +
   geom_line()
 print(p_BA_stem)
 
-################################################################################
-## sensitivity of BA_stand function
-################################################################################
-
-trees_vec = c(1:100,10^(seq(2,4, length.out = 20)))
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+## BA_stand ####
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+trees_vec = c(0:500,10^(seq(2,4, length.out = 20)))
 dbh_vec = dbh = seq(1,300, 1)
 patch_size_vec = seq(0.04, 0.2, 0.02)
 patch_size_vec = c(0.01,0.05,0.08,seq(0.1,1,0.2),1)
-
-
 
 test_dt_out <- data.table()
 i=patch_size_vec[1]
@@ -88,41 +86,23 @@ for(i in patch_size_vec){
   cohort_df1$patch_size_ha = i
   test_dt_out <- rbind(test_dt_out,cohort_df1)
 }
-# Calculate basal area
 
-# Plot
-# p_BA_stand <- ggplot(test_dt_out, aes(x = dbh, y = basal_area)) +
-#   ggtitle("BA_stem", paste0(deparse(BA_stem), collapse = "\n")) +
-#   ylab("BA_stem(dbh)") +
-#   xlab("dbh [cm]") +
-#   geom_line()+
-#   facet_wrap(~cut(trees,breaks = 16))
-
-test_dt_out[basal_area > 100, basal_area := 100,]
-# test_dt_out[, trees2 := cut(trees/patch_size_ha, 10),]
-table(test_dt_out$trees)
-ggplot(test_dt_out[trees_ha <=100],aes(y = factor(trees_ha), x = dbh, fill = basal_area))+
+max_ba = 60
+test_dt_out[basal_area > max_ba, basal_area := max_ba,]
+p_BA_stand <- ggplot(test_dt_out[trees_ha <=500],aes(y = factor(trees_ha), x = dbh, fill = basal_area))+
   geom_tile()+
   ylab("trees [N/ha]")+
   xlab("mean stand dbh [cm]")+
   guides(fill=guide_legend(title="basal area\n[m^2/ha]"))+
-  ggtitle("competition",paste0(deparse(competition),collapse = "\n"))+
-  scale_fill_viridis_c(breaks = seq(0,100,10), labels = c(seq(0,90,10),">100"))+
+  ggtitle("BA_stand",paste0(deparse(BA_stand),collapse = "\n"))+
+  scale_fill_viridis_c(breaks = seq(0,max_ba,10), labels = c(seq(0,max_ba-10,10),paste0(">",max_ba)))+
+  scale_y_discrete(breaks = seq(0,500,100))+
   facet_wrap(~patch_size_ha, labeller = labeller(patch_size_ha = function(x) paste0("patch size = ",x, " ha")))
+print(p_BA_stand)
 
-ggplot(test_dt_out[trees_ha > 100],aes(y = factor(round(trees_ha)), x = dbh, fill = basal_area))+
-  geom_tile()+
-  ylab("trees [N/ha]")+
-  xlab("mean stand dbh [cm]")+
-  guides(fill=guide_legend(title="basal area\n[m^2/ha]"))+
-  ggtitle("competition",paste0(deparse(competition),collapse = "\n"))+
-  scale_fill_viridis_c(breaks = seq(0,100,10), labels = c(seq(0,90,10),">100"))+
-  facet_wrap(~patch_size_ha, labeller = labeller(patch_size_ha = function(x) paste0("patch size = ",x, " ha")))
-# print(p_BA_stand)
-
-# ################################################################################
-# ## sensitivity of compF_P function
-# ################################################################################
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+## competition ####
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
 patch_size = 0.1
 
 trees_vec = c(1:99,10^(seq(2,4, length.out = 16)))
@@ -146,14 +126,12 @@ dbh = cohort$dbh
 species = cohort$species
 trees = cohort$trees
 
-
 comp = competition(cohort$dbh, cohort$species, cohort$trees,
                    parHeight = torch::torch_tensor(0.5), h=0)
 
-
 cohort_df1$light = torch::as_array(comp)[,1,1]
 
-ggplot(cohort_df1,aes(y = factor(trees/0.1), x = dbh, fill = light))+
+ggplot(cohort_df1,aes(y = factor(trees/0.1), x = dbh, fill = light*100))+
   geom_tile()+
   ylab("trees/ha [N]")+
   xlab("mean stand dbh [cm]")+
