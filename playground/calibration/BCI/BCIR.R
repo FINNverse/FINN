700919df7466bf8e29c35e57920b8d34fc9adfb9
ykaeber <y.kaeber@posteo.de>
2025-01-14 13:40:25 +0100

    minor changes

--- playground/calibration/BCI/BCIR.R
+++ playground/calibration/BCI/BCIR.R
@@ -2,100 +2,90 @@ library(data.table)
 library(FINN)
 library(torch)
 
-stand_dt = fread("data/calibration-data/BCI-V3/stand_dt.csv")
-obs_df = fread("data/calibration-data/BCI-V3/obs_df.csv")
-env = fread("data/calibration-data/BCI-V3/env_dt.csv")
-swp_dt = fread("data/calibration-data/BCI-V3/swp_dt.csv")
-stand_dt[census == 1985 & fresh_dead == 0,.(
-  ba = sum(ba),
-  dbh = sum(dbh_cm, na.rm=T)
-),]
-obs_df[census == 1985,.(
-  # ba = sum(ba),
-  dbh = sum(dbh, na.rm=T)
-),]
+stand_dt = fread("processed_data/calibration-data/BCI-stand-data/stand_dt.csv")
+meteo_dt = fread("processed_data/calibration-data/BCI-stand-data/meteo_dt.csv")
+swp_dt = fread("processed_data/calibration-data/BCI-stand-data/swp_dt.csv")
+cohorts_dt = fread("processed_data/calibration-data/BCI-stand-data/initial_cohorts.csv")
 
-colnames(obs_df)
-colnames(stand_dt)[6] = "dbh"
-
-head(stand_dt)
-stand_dt
-stand_dt$AL = NA
-colnames(stand_dt)[c(1,7, 9, 11)] = c("year", "growth", "reg", "mort")
-head(stand_dt)
-data = stand_dt
+obs_dt = stand_dt[,.(
+  # year = census - 1984,
+  year = census,
+  siteID, species,
+  ba = ba,
+  dbh = dbh_cm,
+  trees = trees,
+  growth = g,
+  mort = m,
+  reg = r,
+  AL = NA_real_)]
 
 # fill sites
 # Jedes jahr alle sites!
 empty_res =
-  lapply(unique(data$species), function(sp) {
+  lapply(unique(obs_dt$species), function(sp) {
     empty_sites =
-      lapply(unique(data$year), function(yy) {
-        expand.grid(siteID = setdiff(data$siteID |> unique(), data[species==sp & year == yy]$siteID),
+      lapply(unique(obs_dt$year), function(yy) {
+        expand.grid(siteID = setdiff(obs_dt$siteID |> unique(), obs_dt[species==sp & year == yy]$siteID),
                     year = yy,
                     species = sp
         )
       })
     rbindlist(empty_sites, fill = TRUE)
   })
-data2 = rbindlist(list(data, rbindlist(empty_res, fill = TRUE)), fill = TRUE)
-data = data2
+obs_dt2 = rbindlist(list(obs_dt, rbindlist(empty_res, fill = TRUE)), fill = TRUE)
+obs_dt = obs_dt2
 
-# Env prep.
+# nrow(obs_dt[species == 1])
+# nrow(obs_dt[species == 2])
 
-
-missing_years =
-  lapply(1982:1984, function(year) {
-    tmp = env[2,]
-    tmp$year = year
-    return(tmp)
-  }) |> rbindlist()
+all(table(obs_dt$species) == unique(table(obs_dt$species)))
+all(table(obs_dt$siteID) == unique(table(obs_dt$siteID)))
+all(table(obs_dt$year) == unique(table(obs_dt$year)))
 
 
-
-env = rbindlist(list(missing_years, env[-1,]))
-env = env[!year %in% 2016:2019]
-
-env =
-  lapply(unique(data$siteID), function(site) {
-    tmp = env
+env_dt =
+  lapply(unique(stand_dt$siteID), function(site) {
+    tmp = meteo_dt
     tmp$siteID = site
     return(tmp)
   }) |> rbindlist()
 
-env$Prec = scale(env$Prec)
-env$T_mean = scale(env$T_mean)
-env$T_max = scale(env$T_max)
-env$T_min = scale(env$T_min)
-env$SR_kW_m2 = scale(env$SR_kW_m2)
-env$RH_prc = scale(env$RH_prc)
-env = merge(env, swp_dt[,.(siteID = patch, swp)], by = "siteID")
-data$AL = as.numeric(data$AL)
+env_dt <- merge(env_dt, swp_dt[,.(siteID, swp)], by = "siteID")
+
+env_dt$Prec = scale(env_dt$Prec)
+env_dt$T_mean = scale(env_dt$T_mean)
+env_dt$T_max = scale(env_dt$T_max)
+env_dt$T_min = scale(env_dt$T_min)
+env_dt$SR_kW_m2 = scale(env_dt$SR_kW_m2)
+env_dt$RH_prc = scale(env_dt$RH_prc)
+env_dt$swp = scale(env_dt$swp)
+
+str(env_dt)
+str(obs_dt)
 
 # Preparation of cohorts! BUG!!!!
-cohort1 <- FINN::CohortMat$new(obs_df)
-sp= cohort1$species_r
-sp[sp==0] = 1L
-sp[is.na(sp)] = 1L
-cohort2 = FINN::CohortMat$new(dbh = cohort1$dbh_r, trees = cohort1$trees_r, species = sp, sp = uniqueN(stand_dt$species))
+cohorts_dt <- cohorts_dt[,.(cohortID, species, dbh = round(dbh_cm,4), trees)]
+cohort1 <- FINN::CohortMat$new(dbh = cohorts_dt$dbh, trees = cohorts_dt$trees, species = cohorts_dt$species, sp = uniqueN(obs_dt$species))
+# cohort2 = FINN::CohortMat$new(dbh = cohort1$dbh_r, trees = cohort1$trees_r, species = sp, sp = uniqueN(stand_dt$species))
 # sp zum indexen, aber wenn 0 exisitiert -> seg fault / index fault
 
 # Option A) Train model
-m = finn(env = env, data = data,
+m = finn(env = env_dt,
+         data = obs_dt,
          patches = 1L,
          mortalityProcess = createProcess(~., func = mortality, optimizeSpecies = TRUE, optimizeEnv = TRUE),
          growthProcess = createProcess(~., func = growth, optimizeSpecies = TRUE, optimizeEnv = TRUE),
          regenerationProcess = createProcess(~., func = regeneration, optimizeSpecies = TRUE, optimizeEnv = TRUE),
          device = "gpu",
          optimizeHeight = TRUE,
-         init = cohort2,
+         init = cohort1,
          lr=0.01,
          epochs = 1,
          patch_size = 0.1,
-         batchsize = 350L,
+         batchsize = 350L,sp = uniqueN(obs_dt$species),
          weights = c(1, 0.1, 3, 1.5, 3, 1.0))
 
-# m2 = finn(env = env, data = data,
+# m2 = finn(env = env_dt, data = obs_dt,
 #           mortalityProcess = createProcess(~., func = mortality, optimizeSpecies = TRUE, optimizeEnv = TRUE, initSpecies = m$model$parMortTR, initEnv = m$model$parMortEnv_r),
 #           growthProcess = createProcess(~., func = growth, optimizeSpecies = TRUE, optimizeEnv = TRUE, initSpecies = m$model$parGrowthTR, initEnv = m$model$parGrowthEnv_r),
 #           regenerationProcess = createProcess(~., func = regeneration, optimizeSpecies = TRUE, optimizeEnv = TRUE, initSpecies = m$model$parRegTR, initEnv = m$model$parRegEnv_r),
@@ -157,7 +147,7 @@ cohort1 <- FINN::CohortMat$new(obs_df, sp = 195)
 pdat = data.table()
 for(i in 1:10) {
 predictions =
-  simulateForest(env = env,
+  simulateForest(env = env_dt,
                  height = as.vector(par_backtr$parHeight),
                  sp = nrow(par_backtr$parReg),
                  init = cohort1,
